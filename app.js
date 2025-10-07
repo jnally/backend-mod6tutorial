295c5a92837c63a0a07505e87dffca8737690fe6
Jeremy Nally <jnally3@ivytech.edu>
2025-10-06 22:00:26 -0400

    I think it's working now - jn

--- app.js
+++ app.js
@@ -1,7 +1,87 @@
 const express = require("express");
+const Song = require("./models/song");
+var cors = require('cors')
+
+const app = express();
+app.use(cors());
+
+// Middleware that parses HTTP requests with JSON body
+app.use(express.json());
+
+const router = express.Router();
+
+// Get list of all songs in the database
+router.get("/songs", async(req,res) =>{
+   try{
+      const songs = await Song.find({});
+      res.send(songs);
+      console.log(songs);
+   }
+   catch (err){
+      console.log(err);
+   }
+
+})
+
+router.post("/songs", async(req,res) =>{
+   try{
+      const song = await new Song(req.body);
+      await song.save();
+      res.status(201).json(song);
+      console.log(song);
+   }
+   catch(err){
+      res.status(400).send(err);
+   }
+});
+
+
+app.use("/api", router);
+
+app.listen(3000);
+
+
+/* const express = require("express");
+const app = express();
+
+// Middleware that parses HTTP requests with JSON body
+app.use(express.json());
+
+const Song = require("./models/song");
+const router = require("express").Router();
+
+// Get list of all songs in the database
+router.get("/", async function(req, res) {
+   try {
+      const songs = await Song.find();
+      res.json(songs);
+   }
+   catch (ex) {
+      res.status(400).send(ex.message);
+   }
+});
+
+// Add a new song to the database
+router.post("/", async function(req, res) {
+   try {
+      const song = new Song(req.body);
+      await song.save();
+      res.status(201).json(song);
+   }
+   catch (ex) {
+      res.status(400).send(ex.message);
+   }
+});
+
+app.listen(3000);*/
+
+
+
+
+/* const express = require("express");
 var cors = require("cors");
 const bodyParser = require("body-parser");
-const Song = require("./models/songs");
+const Song = require("./models/song");
 
 const app = express();
 app.use(cors());
@@ -31,3 +111,4 @@ router.get("/songs", function(req,res){
 
 app.use("/api", router);
 app.listen(3000);
+*/
--- db.js
+++ db.js
@@ -1,5 +1,6 @@
-const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://jnally:lampcupdoor@songdb.zlm1mv2.mongodb.net/?retryWrites=true&w=majority&appName=SongDB",{useNewURLParser: true});
+const mongoose = require("mongoose");
+mongoose.connect("mongodb+srv://jnally:lampcupdoor@songdb.zlm1mv2.mongodb.net/?retryWrites=true&w=majority",
+    {useNewUrlParser: true});
 
 module.exports = mongoose;
 
