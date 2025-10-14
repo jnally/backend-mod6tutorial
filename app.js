1abc0fcc28037134928394afb4ed919317e695c2
Jeremy Nally <jnally3@ivytech.edu>
2025-10-13 22:33:21 -0400

    new updates from mod 7 tutorial - jn

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const Song = require("./models/song");
+const User = require("./models/user");
+
 var cors = require('cors')
 
 const app = express();
@@ -9,6 +11,60 @@ app.use(cors());
 app.use(express.json());
 
 const router = express.Router();
+
+const secret = "thesecret";
+
+router.post("/user", async(req,res) =>{
+   if(!req.body.username || !req.body.password){
+      res.status(400).json({error: "Missing username or passwword"})
+   }
+
+   //create a hash to encrypt the password
+   const hash = bcrypt.hashSync(req.body.password, 10)
+   const newUser = await new User({
+      username: req.body.username,
+      password: hash,
+      status: req.body.status
+   })
+   try{
+      await newUser.save()
+      res.sendStatus(201) //created
+   }
+   catch(err){
+      res.status(400).send
+   }
+});
+
+
+router.post("/auth", async(req,res) =>{
+   if(!req.body.username || !req.body.password){
+      res.status(401).json({error: "Missing username or password"})
+      return
+   }
+   //find the user in the database
+   try{
+      const user = await User.findOne({username: req.body.username})
+      if (!user){
+         res.status(401).json({error: "User not found"})
+     
+      }
+      else{
+         //check the username and password to see if they match
+        if(bcrypt.compareSync(req.body.password,user.password)){
+            //create a token
+            const token = jwt.encode({username: user.username}, secret)
+            res.json({token: token, username: user.username, userID: user._id})
+         }
+         else{
+            res.status(401).json({error: "Invalid password"})
+         }
+      }
+   }
+   catch(err){
+      res.status(400).send(err.message)
+   }
+});
+
 
 // Get list of all songs in the database
 router.get("/songs", async(req,res) =>{
@@ -21,7 +77,17 @@ router.get("/songs", async(req,res) =>{
       console.log(err);
    }
 
-})
+});
+
+router.get("/songs/:id", async (req,res) =>{
+   try{
+      const song = await Song.findById(req.params.id)
+      res.json(song)
+   }
+   catch (err){
+      res.status(400).send(err)
+   }
+});
 
 router.post("/songs", async(req,res) =>{
    try{
@@ -32,6 +98,46 @@ router.post("/songs", async(req,res) =>{
    }
    catch(err){
       res.status(400).send(err);
+   }
+});
+
+router.put("/songs/:id", async(req,res) =>{
+   try{
+      const song = req.body
+      await Song.updateOne({_id: req.params.id},song)
+      console.log(song)
+      res.sendStatus(204)
+
+
+   }
+   catch(err){
+      res.status(400).send(err)
+   }
+});
+
+router.delete("/songs/:id", async(req,res) =>{
+   try{
+      const song = await Song.findById(req.params.id)
+      console.log(song)
+      await Song.deleteOne({_id: song._id})
+      res.sendStatus(204) //deleted status code
+   }
+   catch(err){
+      res.status(400).send(err)
+   }
+});
+
+router.put("/playlist", async(req,res) =>{
+   try{
+      const user = await User.findById(req.body.userID)
+      await user.updateOne({$push: {playlist: req.body.songID}})
+      console.log(user)
+      
+      
+      res.sendStatus(204)
+   }
+   catch(err){
+      res.status(400).send(err)
    }
 });
 
